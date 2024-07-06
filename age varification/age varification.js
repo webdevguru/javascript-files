75e425cdde6573fc981e48eba16002d12ea0a2b9
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-07-06 14:07:04 +0530

    Create age varification.js

---
+++ age varification/age varification.js
@@ -0,0 +1,15 @@
+let a=10;
+let b=11;
+let c= 18;
+if(a<=c)
+    {
+        console.log('a is not eligible for this website' );
+    }
+if(b<=c)
+    {
+        console.log('b is not eligible for this website' );
+    }
+if(c>b<=a)
+    {
+        console.log('c is eligible for this website' );
+    }
