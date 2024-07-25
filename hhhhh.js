5622f54099164f9b6934903748c643fe3e420cb5
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-07-25 23:50:07 +0530

    Create hhhhh.js

---
+++ hhhhh.js
@@ -0,0 +1,12 @@
+// Anonymous function expression
+var multiply = function(a, b) {
+    return a * b;
+};
+
+// Usage
+console.log(multiply(2, 3)); // Output: 6
+
+// Named function expression
+var multiply = function multiplyFunction(a, b) {
+    return a * b;
+};
