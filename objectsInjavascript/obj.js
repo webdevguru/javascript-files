56ca9b15c62529a10c0f1deec6cb748b0ef45a26
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-08-01 18:32:40 +0530

    Create obj.js

---
+++ objectsInjavascript/obj.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript Objects</h1>
+<p>Displaying a JavaScript object will output [object Object]:</p>
+
+<p id="demo"></p>
+
+<script>
+// Create an Object
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+// Display Object
+document.getElementById("demo").innerHTML = person;
+</script>
+
+</body>
+</html>
