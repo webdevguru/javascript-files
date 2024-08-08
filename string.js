b7e4919e82aae64271e11a2d974c152ff650eac5
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-08-08 23:16:02 +0530

    Add files via upload

--- string.js
+++ string.js
@@ -1,1 +1,9 @@
+// const name = "Sourav";
+// const repocount = 27;
 
+// console.log(name + repocount + "value");
+
+// console.log(`Hello, my name is ${name} and my repocount is ${repocount}`);
+
+const gamename = new String('sourav');
+console.log(gamename[0]); // Output: 's'
