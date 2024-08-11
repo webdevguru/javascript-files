d7da4785c6a22e5877c57ecb3fe3765479028ba8
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-08-11 23:58:48 +0530

    Create  Functions.js

---
+++ functions In javascript/Functions.js
@@ -0,0 +1,12 @@
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+console.log(greet("Alice")); // Outputs: "Hello, Alice!"
+
+
+const greet = name => {
+  return "Hello, " + name + "!";
+};
+
+console.log(greet("Alice")); // Outputs: "Hello, Alice!"
