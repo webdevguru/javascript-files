711156032643f7aa456eabe6497e7c43d4fc8548
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-08-20 22:38:04 +0530

    Add files via upload

---
+++ javascriptobjects.js
@@ -0,0 +1,21 @@
+// how to create objects//
+const person ={
+    Name : "sourav",
+
+     age :27 ,
+     hobbies :["guitar","Slepping", "listening myusic"], 
+}
+console.log (person);
+//how to access data from objects 
+console.log(person.name)
+console.log(person.age)
+console.log(person.hobbies)
+//how  to add key value pair to objects
+//using . operator
+person.age= 27;
+person.hobbies= 27;
+
+person.log (person);
+// or we can use bracket
+person ["person"]="male";
+console.log(person);
