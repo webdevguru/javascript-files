c682561c6d0ffbb67d032d02126770aac1c2a2cf
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-08-31 20:51:56 +0530

    Create object.js

---
+++ updating key/object.js
@@ -0,0 +1,11 @@
+const key ="email"
+const person = {
+    name :"harshit",
+    age : 27,
+    "perosn hobbies": ["guitar", "sleeping","listening"]
+
+}
+person.key="sanyalsourav55@gmail.com"// here were are getting the output of key as sanyalsourav55@gmailcom but we neare not updating it if we want to update it  then
+//
+// person[key]="sanyalsourav55@gmail.com" ( for updating our email)
+console.log(person);
