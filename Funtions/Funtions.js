704bf986b1cbd9d34c7ebe9101e3d3d60b195803
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-09-01 10:05:22 +0530

    Create Funtions.js

---
+++ Funtions/Funtions.js
@@ -0,0 +1,13 @@
+function singHappyBirthday() {
+    console.log("Happy Birthday");
+}
+
+function sum_of_two_Numbers(Number1, Number2) {
+    return Number1 + Number2;
+}
+
+// Example usage:
+singHappyBirthday(); // Output: "Happy Birthday"
+
+const sum = sum_of_two_Numbers(3, 5);
+console.log(sum); // Output: 8
