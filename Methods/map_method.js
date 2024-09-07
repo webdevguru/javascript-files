045c26f54b6fe934172c394232cf2318592cfba8
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-09-07 15:02:36 +0530

    Add files via upload

---
+++ Methods/map_method.js
@@ -0,0 +1,18 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const squares = numbers.map((number) => {
+    return number * number;
+});
+
+console.log(squares); // Output: [1, 4, 9, 16, 25]
+//
+
+let arun = ["singer","poet","dancer","Engineer"];
+
+const   WhoIsArun =arun.map((role)=>{
+
+    return role;
+}
+ 
+)
+console.log(WhoIsArun);
