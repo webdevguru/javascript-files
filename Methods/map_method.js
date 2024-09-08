a9ea76360cdad2f247d420331a5fe2da2a8da252
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-09-08 12:34:58 +0530

    Update map_method.js

--- Methods/map_method.js
+++ Methods/map_method.js
@@ -16,3 +16,28 @@ const   WhoIsArun =arun.map((role)=>{
  
 )
 console.log(WhoIsArun);
+
+
+const numbers = [2, 3, 4, 6, 7];
+
+const square = function(number) {
+  return number * number;
+};
+
+// Use map to apply the square function to each element of the array
+// const squaredNumbers = 
+// numbers.map(square);
+
+// console.log(squaredNumbers);  // Output: [4, 9, 16, 36, 49]
+console.log(numbers.map(square));
+
+
+
+const numbers = [1,2,3,4,5,6,7];
+
+const square = function(number) {
+    return number * number; // Use 'number', the function parameter
+  };
+  
+
+console.log(numbers.map(square));
