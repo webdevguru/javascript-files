4c3d577f1c2db64e79ff5cddf0593f1823043a58
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-09-08 12:45:45 +0530

    Update map method.js

--- Methods/map method.js
+++ Methods/map method.js
@@ -1,1 +1,15 @@
+const souravArray = [
+  { hobby: "singer" },
+  { hobby: "dancer" },
+  { hobby: "writer" }
+];
+
+const updatedArray = souravArray.map(person => {
+  return {
+    ...person, // Spread the existing properties
+    profession: "Engineer" // Add or override properties
+  };
+});
+
+console.log(updatedArray);
 
