35367d8a5961e0e82a2ccf076721695bdfed110a
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-09-11 22:35:34 +0530

    Create optional chaining.js

---
+++ optiona chaining/optional chaining.js
@@ -0,0 +1,30 @@
+const user = {
+    firstname: "sourav",
+    lastname: "sanyal",
+    address: { houseeNumber: '688/1,Ashoknagar' },
+    Degree: "Electronics and Communication Engineering",
+    profession: "Mern stack Developer",
+    Father_name: "Malay Ranjan sanayal",
+    Mother_name: "Minu sanyal",
+    Elder_Brother_Name: "Sudipto Sanyal",
+    Grand_Father_Name: "Nihar ranjan sanyal",
+    Grand_Mother_Name: "Mira Sanyal",
+    Great_Grand_Father_Name: "Rajnai Ranjan sanyal",
+    Great_Grand_Mother_Name: "Ganoda_Sundori_ Devi",
+}
+
+console.log(user.firstname);
+console.log(user.lastname);
+console.log(user.address.houseeNumber);
+console.log(user.Degree);
+console.log(user.profession);
+console.log(user.Father_name);
+console.log(user.Mother_name);
+console.log(user.Elder_Brother_Name);
+console.log(user.Grand_Father_Name);
+console.log(user.Grand_Mother_Name);
+console.log(user.Great_Grand_Father_Name);
+console.log(user.Great_Grand_Mother_Name);
+
+
+
