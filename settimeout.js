16bb345f47af556f34b76926536e306226ff204a
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-09-28 19:21:43 +0530

    Create settimeout.js

---
+++ settimeout.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+     console.log("hey how are you");
+     
+}, 2000);
+
+ setTimeout((hello)=>{
+
+    console.log ("i am fine");
+ },5000);
+
+
+
+ setTimeout(() => {
+    console.log( "i hope you are doing good ");
+ }, 1000);
+
